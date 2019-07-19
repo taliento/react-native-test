d5b37e2f99441bca6602151e3fa5d07ac8b193e2
taliento <dave@taliento-nb.lan>
2019-07-19 12:24:40 +0200

    first commit

---
+++ TabNavigation.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Text, View  } from 'react-native';
+import { createMaterialTopTabNavigator, createAppContainer  } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'  }}>
+      <Text>Home!</Text>
+      </View>
+
+    );
+
+  }
+
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'  }}>
+      <Text>Settings!</Text>
+      </View>
+
+    );
+
+  }
+
+}
+
+class InfoScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'  }}>
+      <Text>Infos!</Text>
+      </View>
+
+    );
+
+  }
+
+}
+
+const TabNavigator = createMaterialTopTabNavigator(
+  {
+    Home: HomeScreen,
+    Settings: SettingsScreen,
+    InfosScreen: InfoScreen
+  },
+  {
+    swipeEnabled: true,
+    animationEnabled: true,
+    tabBarOptions: {
+      scrollEnabled: true,
+      labelStyle: {
+        fontSize: 12,
+      },
+      tabStyle: {
+        width: 200,
+
+      },
+      style: {
+        backgroundColor: 'blue',
+
+      }
+    }
+  }
+);
+
+export default createAppContainer(TabNavigator);
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Touchables from './Touchables';
+import {name as appName} from './app.json';
+import TabNavigation from './TabNavigation';
+
+
+AppRegistry.registerComponent(appName, () => TabNavigation);
